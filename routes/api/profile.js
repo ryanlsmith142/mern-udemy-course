73ef3581308721d3dcaec94534cbacb76435f37e
Ryan Smith <ryan.l.smith142@gmail.com>
2020-12-29 19:42:34 -0600

    Allows logged in user to get profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User')
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name, avatar']);
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile Route'));
+        if(!profile) {
+            return res.status(400).json( { msg: 'There is no profile for this user'});
+        }
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error')
+    }
+});
 
 module.exports = router;
